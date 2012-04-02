bfc6b630fed84f5ea9546e00a251f09bc85a69be
Randy Yates <lpcustom@gmail.com>
2012-04-02 13:24:18 -0400

    Added the login configuration, after much debate as to whether to put this in a database table

--- includes/config.php
+++ includes/config.php
@@ -4,16 +4,23 @@
  * @author lpcustom 
  * Configuration file for twurlo.
  */
-/* Database configuration
- * ----------------------
+
+/************************Database configuration*********************************
  * We currently support MySQL. We plan to support PostgreSQL in a later release.
  * YOU MUST CREATE THE MYSQL DATABASE BEFORE YOU CAN CONNECT TO IT.
- */
+ *******************************************************************************/
+$config['db_type'] = "mysql";	    // current only supports mysql
+$config['db_name'] = "twurlo";	    // name of the database you created
+$config['db_host'] = "localhost";   // hostname of db server, usually localhost
+$config['db_user'] = "twurlo";	    // name of the database user you created
+$config['db_pass'] = "twurlo";	    // password for the database user
+/*******************************************************************************/
 
-/* MySQL */
-$config['db_type'] = "mysql";
-$config['db_name'] = "twurlo";
-$config['db_host'] = "localhost";
-$config['db_user'] = "twurlo";
-$config['db_pass'] = "twurlo";
+/************************Admin user infomration*********************************
+ * CHANGE THIS!!!
+ * THIS IS YOUR LOGIN INFORMATION TO MANAGE THE SITE
+ *******************************************************************************/
+$config['username'] = "admin";
+$config['password'] = "admin";
+/*******************************************************************************/
 ?>
