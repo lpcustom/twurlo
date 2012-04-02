5c4dc64e0e08f83856d44ff9ba18d7f8b254792e
Randy Yates <lpcustom@gmail.com>
2012-04-02 10:10:46 -0400

    fixed refactor

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
  * 
  **/
 
-require_once 'inc/config.php';
-require_once 'inc/Database.php';
+require_once 'includes/config.php';
+require_once 'includes/Database.php';
 $db = new Database($config);
 if(!$db->checkInitialized()) {
     die("Database isn't initialized. You need to setup your database in manage.php.");
