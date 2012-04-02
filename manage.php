2c5f0d7c44e9a3bf8e8dba3e23e42f8157439cb4
Randy Yates <lpcustom@gmail.com>
2012-04-02 14:30:05 -0400

    fixed login communicate link to add_link in manage.php

--- manage.php
+++ manage.php
@@ -12,9 +12,17 @@ if(!$db->checkInitialized()) {
 	die("Unable to initialize database. Check your configuration.");
     }
 }
+if(isset($_REQUEST['a'])) { $add_link = $_REQUEST['a']; }
+
 if(!isset($_SESSION['username'])) {
-    header("Location: login.php");
+    if(isset($add_link)) {
+	header("Location: login.php?a=" . $add_link);
+    }
+    else {
+	header("Location: login.php");
+    }
 }
+
 ?>
 <!doctype html>
 <html>
@@ -28,9 +36,24 @@ if(!isset($_SESSION['username'])) {
     <body>
         <div id="body_wrapper">
             <div id="heading_wrapper">	
+		<h2>Welcome to your Twurlo Management Console</h2>
             </div>
             <div id="content_wrapper">
-		
+		<form action="add.php" method="post">
+		    <table id="add_link">
+			<tr>
+			    <td>add a link</td>
+			    <td colspan="3"><input type="text" name="url" id="url" size="80" value="<?php echo (isset($add_link) && $add_link != "") ? $add_link : "" ;?>"></td>
+			</tr>
+			<tr>
+			    <td>custom short name</td>
+			    <td><input type="text" name="short" id="short" /></td>
+			    <td>(leave blank to generate automatically)</td>
+			    <td><input class="pad5" style="padding: 5px 30px;" type="submit" value="add" /></td>
+			</tr>
+			
+		    </table>
+		</form>
             </div>
             <div id="footer_wrapper">
 
