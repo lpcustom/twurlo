2d143466fc8e8addacd621bbaedcd043cc4f8af1
Randy Yates <lpcustom@gmail.com>
2012-04-02 10:04:19 -0400

    Added layout html to manage.php

--- manage.php
+++ manage.php
@@ -4,7 +4,31 @@
  */
 require_once 'inc/config.php';
 require_once 'inc/Database.php';
-
-
-
+$installed = false;
+$db = new Database($config);
+if(!$db->checkInitialized()) {
+    if(!$db->initDB()) {
+        die("Couldn't initialize database. Verify that your configuration is correct.");
+    }
+}
+else {
+    $installed = true;
+}
 ?>
+<!doctype html>
+<html>
+    <head>
+        <title>
+            Twurlo Management Console
+        </title>
+        <link rel="stylesheet" href="css/reset.css" />
+        <link rel="stylesheet" href="css/twurlo.css" />
+    </head>
+    <body>
+        <div id="body_wrapper">
+            <div id="heading_wrapper">
+                
+            </div>
+        </div>
+    </body>        
+</html>
