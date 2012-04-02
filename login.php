8a64cb97e8be50c08b08f3f9ecc9c6d917bb6592
Randy Yates <lpcustom@gmail.com>
2012-04-02 12:47:03 -0400

    Added initial login form and styles.

--- login.php
+++ login.php
@@ -1,9 +1,51 @@
-<?php ?>
+<?php
+session_start();
 
+require_once 'includes/config.php';
+require_once 'includes/Database.php';
+$db = new Database($config);
+if(isset($_POST['username']) && isset($_POST['password'])) {
+    if($db->authenticate($_POST['username'], $_POST['password'])) {
+	$_SESSION['username'] = $db->getConfItem("username");
+	header("Location: manage.php");
+    }
+    else {
+	$error_message = "username/password combination incorrect";
+    }
+}
+?>
 <html>
     <head>
-        <title>
-
-        </title>
+        <title>Login to Twurlo</title>
+	<link rel="stylesheet" href="css/reset.css" />
+	<link rel="stylesheet" href="css/twurlo.css" />
     </head>
+    
+    <body>
+	<div id="login_wrapper">
+	    <form action="" method="post">
+		<table id="login">
+		    <tr>
+			<td colspan="2" class="bold center">login to twurlo</td>
+		    </tr>
+		    <tr>
+			<td>username</td>
+			<td><input type="text" name="username" id="username" /></td>
+		    </tr>
+		    <tr>
+			<td>password</td>
+			<td><input type="password" name="password" id="password" /></td>
+		    </tr>
+		    <tr>
+			<td colspan="2" class="center">
+			    <input style="padding: 5px 30px; " type="submit" value="login" />
+			</td>
+		    </tr>
+		    <tr>
+			<td colspan="2" class="center"><?php if(isset($error_message)) { echo($error_message); }?></td>
+		    </tr>
+		</table>
+	    </form>
+	</div>
+    </body>
 </html>
