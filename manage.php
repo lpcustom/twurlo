c984393a7a4ef0aa7dbfb194d10e8359900c5858
Randy Yates <lpcustom@gmail.com>
2012-04-02 15:31:24 -0400

    Added logic to fetch links for link list

--- manage.php
+++ manage.php
@@ -12,17 +12,38 @@ if(!$db->checkInitialized()) {
 	die("Unable to initialize database. Check your configuration.");
     }
 }
-if(isset($_REQUEST['a'])) { $add_link = $_REQUEST['a']; }
+if(isset($_REQUEST['a'])) {
+    $add_link = $_REQUEST['a'];
+}
 
 if(!isset($_SESSION['username'])) {
     if(isset($add_link)) {
 	header("Location: login.php?a=" . $add_link);
-    }
-    else {
+    } else {
 	header("Location: login.php");
     }
 }
-
+if(isset($_REQUEST['s'])) {
+    $sort = $_REQUEST['s'];
+} else {
+    $sort = "date";
+}
+if(isset($_REQUEST['p'])) {
+    $page = $_REQUEST['p'];
+} else {
+    $page = "1";
+}
+if(isset($_REQUEST['s'])) {
+    $search = $_REQUEST['s'];
+} else {
+    $search = "";
+}
+if(isset($_REQUEST['d'])) {
+    $direction = $_REQUEST['d'];
+} else {
+    $direction = "DESC";
+}
+$links = $db->getLinks($sort, $direction, $page, $search);
 ?>
 <!doctype html>
 <html>
@@ -43,7 +64,7 @@ if(!isset($_SESSION['username'])) {
 		    <table id="add_link">
 			<tr>
 			    <td>add a link</td>
-			    <td colspan="3"><input type="text" name="url" id="url" size="80" value="<?php echo (isset($add_link) && $add_link != "") ? $add_link : "" ;?>"></td>
+			    <td colspan="3"><input type="text" name="url" id="url" size="80" value="<?php echo (isset($add_link) && $add_link != "") ? $add_link : ""; ?>"></td>
 			</tr>
 			<tr>
 			    <td>custom short name</td>
@@ -51,9 +72,24 @@ if(!isset($_SESSION['username'])) {
 			    <td>(leave blank to generate automatically)</td>
 			    <td><input class="pad5" style="padding: 5px 30px;" type="submit" value="add" /></td>
 			</tr>
-			
 		    </table>
 		</form>
+		<?php if(isset($links) && $links !== false && count($links) > 0): ?>
+    		<table id="link_list">
+    		    <tr>
+    			<td class="center">timestamp</td>
+    			<td>destination</td>
+    			<td>short name</td>
+    		    </tr>
+		    <?php foreach($links as $link): ?>
+		    <tr>
+			<td><?php echo $link['timestamp']; ?></td>
+			<td><?php echo $link['destination']; ?></td>
+			<td><?php echo $config['baseurl'] . $link['shortname']; ?></td>
+		    </tr>
+		    <?php endforeach; ?>
+    		</table>
+		<?php endif; ?>
             </div>
             <div id="footer_wrapper">
 
