86e0adedea88da2339f3fe46a276796530ddeff1
Randy Yates <lpcustom@gmail.com>
2012-04-05 19:07:21 -0400

    Added link redirection and click recording

--- index.php
+++ index.php
@@ -10,5 +10,26 @@ require_once 'includes/config.php';
 require_once 'includes/Database.php';
 $db = new Database($config);
 
+$referrer = $_SERVER['HTTP_REFERER'];
 
+if(!isset($_REQUEST['l'])) {
+    die("No redirect link specified.");
+} else {
+    $short = $_REQUEST['l'];
+}
+
+if($short !== false) {
+    $link = $db->getLinkByShort($short);
+    $db->recordClick($link, $referrer);
+    switch ($config['redirect_type']) {
+	case "302":
+	    header("Location: " . $link['destination']);
+	    break;
+	case "301":
+	    header('HTTP/1.1 301 Moved Permanently');
+	    header("Location: " . $link['destination']);
+	    break;
+    }
+    
+}
 ?>
