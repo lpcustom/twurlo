3c95aa1a522233588875cf2e4c8be52f65eb8cc5
Randy Yates <lpcustom@gmail.com>
2012-04-05 21:44:19 -0400

    Wrapped some PDO in try catch to log errors and added method to delete links

--- includes/Database.php
+++ includes/Database.php
@@ -62,31 +62,82 @@ class Database {
 	    ":destination"  => $destination));
     }
     
+    public function deleteLink($id) { 
+	try {
+	    $q = "DELETE FROM `links` WHERE `id` = :id;";
+	    $query = $this->db->prepare($q);
+	    if($query->execute(array(":id" => $id))) {
+		$q = "DELETE FROM `clicks` WHERE `link_id` = :id;";
+		$query = $this->db->prepare($q);
+		if($query->execute(array(":id" => $id))) {
+		    return true;
+		}
+		else { return false; }
+	    } else { return false; }
+	}
+	catch(PDOException $ex) {
+	    error_log($ex);
+	    return false;
+	}
+    }
+    
     // get a list of links based on given criteria
     public function getLinks($page = 1, $search = "") {
-	// @todo - add try/catch to this
-	$offset = (($page - 1) * 20);
-	if(isset($search) && $search != "") {
-	    $search = '%' . strtolower($search) . '%';
-	    $q = "SELECT * FROM `links` WHERE LOWER(`destination`) LIKE :search1 OR LOWER(`shortname`) LIKE :search2 ORDER BY `timestamp` DESC LIMIT :offset, 20;";
-	    $query = $this->db->prepare($q);
-	    $query->bindParam(":search1", $search, PDO::PARAM_STR);
-	    $query->bindParam(":search2", $search, PDO::PARAM_STR);
-	    $query->bindParam(":offset", $offset, PDO::PARAM_INT);
-	    $query->execute();	    
+	try {
+	    $offset = (($page - 1) * 20);
+	    if(isset($search) && $search != "") {
+		$search = '%' . strtolower($search) . '%';
+		$q = "SELECT * FROM `links` WHERE LOWER(`destination`) LIKE :search1 OR LOWER(`shortname`) LIKE :search2 ORDER BY `timestamp` DESC LIMIT :offset, 20;";
+		$query = $this->db->prepare($q);
+		$query->bindParam(":search1", $search, PDO::PARAM_STR);
+		$query->bindParam(":search2", $search, PDO::PARAM_STR);
+		$query->bindParam(":offset", $offset, PDO::PARAM_INT);
+		$query->execute();	    
+	    }
+	    else {
+		$q = "SELECT * FROM `links` ORDER BY `timestamp` DESC LIMIT :offset, 20;";
+		$query = $this->db->prepare($q);
+		$query->bindParam(":offset", $offset, PDO::PARAM_INT);
+		$query->execute();	    
+	    }
+	    $results = $query->fetchAll();
+	    if($results !== false && count($results) > 0) {
+		return $results;
+	    }
+	    else {
+		return false;
+	    }
 	}
-	else {
-	    $q = "SELECT * FROM `links` ORDER BY `timestamp` DESC LIMIT :offset, 20;";
-	    $query = $this->db->prepare($q);
-	    $query->bindParam(":offset", $offset, PDO::PARAM_INT);
-	    print_r($query);
-	    $query->execute();	    
+	catch(PDOException $ex) {
+	    error_log($ex);
+	    return false;
 	}
-	$results = $query->fetchAll();
-	if($results !== false && count($results) > 0) {
-	    return $results;
+    }
+    
+    public function getLinkCount($search = "") {
+	try {
+	    if($search != "") {
+		$q = "SELECT COUNT(`id`) AS `count` FROM `links` WHERE LOWER(`destination`) LIKE :search1 or LOWER(`shortname`) LIKE : search2;";
+		$query = $this->db->prepare($q);
+		$query->bindParam(":search1", $search, PDO::PARAM_STR);
+		$query->bindParam(":search2", $search, PDO::PARAM_STR);
+		$query->execute();
+	    }
+	    else {
+		$q = "SELECT COUNT(`id`) AS `count` FROM `links`;";
+		$query = $this->db->prepare($q);
+		$query->execute();
+	    }
+	    $results = $query->fetchAll();
+	    if($results !== false && count($results) > 0) {
+		return $results[0]['count'];
+	    }
+	    else {
+		return 0;
+	    }
 	}
-	else {
+	catch(PDOException $ex) {
+	    error_log($ex);
 	    return false;
 	}
     }
